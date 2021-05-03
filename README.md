f1b93a269f46b0c681aa2cefc1dbafbad5faabd1
Gov <info@govindmohan.com>
2021-05-03 00:12:42 -0400

    wip: add qcustomplot

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Currently contains a Mandelbrot implementation. To come: Julia sets and cool ML.
 
 ## Building
-- Install `cmake`
+- Install `cmake` and [`qcustomplot`](https://www.qcustomplot.com/)
 - `mkdir build && cd build`
 - `cmake ..`
 - `make`
--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,3 +1,20 @@
+#include <iostream>
+
+#include <qcustomplot.h>
+
+//QT5
+#include <QApplication>
+#include <QFileDialog>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QMessageBox>
+
 #include "../include/fractals.hpp"
 
 int main() {
@@ -7,4 +24,9 @@ int main() {
         std::cout << i << " ";
     }
     std::cout << "}" << std::endl;
+
+    QCustomPlot custom_plot;
+    custom_plot.addGraph();
+    custom_plot.graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
+    custom_plot.graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
 }

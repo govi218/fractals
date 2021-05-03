52e8aff7e07830d0abc260634b758bf67f7e2429
Pouya Lajevardi <pouya.lajevardi@mail.utoronto.ca>
2021-05-03 16:02:22 -0400

    Initial Push

--- include/fractals.hpp
+++ include/fractals.hpp
@@ -2,11 +2,14 @@
 #include <functional>
 #include <iostream>
 #include <vector>
+#include <thread>
 
 namespace fractals {
 
 using ComplexDouble = std::complex<double>;
 using FractalSet = std::vector<ComplexDouble>;
+
+FractalSet m_set;
 
 ComplexDouble mandelbrot_process(ComplexDouble c, int boundary = 2,
                                  int max_iterations = 512) {
@@ -23,19 +26,39 @@ ComplexDouble mandelbrot_process(ComplexDouble c, int boundary = 2,
     return z;
 }
 
-FractalSet mandelbrot() {
-    double step = 0.01;
-    double start = -2.0;
-    double end = 2.0;
-    FractalSet m_set;
+void mandelbrot(double start_val, double end_val, double step_val) {
+    double step = step_val; // 0.01;
+    double start = start_val; //-2.0;
+    double end = end_val; //2.0;
+    // FractalSet m_set;
     for (double i = start; i < end; i += step) {
         for (double j = start; j < end; j += step) {
             ComplexDouble c(i, j);
             m_set.push_back(mandelbrot_process(c));
-        }
+            }
     }
-    return m_set;
-}
+                
 
+    // return m_set;
 }  // namespace fractals
 
+
+FractalSet thread(){
+    // m_set.push_back(0);
+    // Pre loop thread setup
+    int num_threads = std::thread::hardware_concurrency(); 
+    std::vector<std::thread> threads;
+    // std::mutex critical;
+    std::cout << num_threads << std::endl;
+    for(int t=0; t<num_threads; t++) {
+        double start_here = -2.0+(t*4/num_threads);
+        double end_here = -2.0+((t+1)*4/num_threads);
+        threads.push_back(std::thread(mandelbrot, start_here, end_here, 0.01));
+        threads[t].join();
+    }
+    // std::thread first(mandelbrot, -2.0, 2.0, 0.01, m_set);
+    // first.join();
+
+    return m_set;
+}
+}
--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <thread>
+#include <fstream>
+#include <complex>
 
-#include<fstream>
 
 #include "../external/qcustomplot/qcustomplot.h"
 
@@ -19,16 +21,18 @@
 
 #include "../include/fractals.hpp"
 
+using ComplexDouble = std::complex<double>;
+using FractalSet = std::vector<ComplexDouble>;
+
 int main() {
-    auto m_set = fractals::mandelbrot();
-    std::cout << "{ ";
-    for (auto i : m_set) {
-        std::cout << i << " ";
-    }
-    std::cout << "}" << std::endl;
+    FractalSet m_set = fractals::thread();
+    // std::cout << "{ ";
+    // for (auto i : m_set) {
+    //     std::cout << i << " ";
+    // }
+    // std::cout << "}" << std::endl;
 
-
-// To output the set (vector m_set) into a text file
+    // To output the set (vector m_set) into a text file
     std::ofstream file;
 	file.open("m_set.txt");
 	for(int i=0;i<m_set.size();++i){
@@ -40,5 +44,6 @@ int main() {
     // custom_plot.addGraph();
     // custom_plot.graph(0)->setPen(QPen(Qt::blue)); // line color blue for
     // first graph custom_plot.graph(0)->setBrush(QBrush(QColor(0, 0, 255,20))); // first graph will be filled with translucent blue
+    return 0;
 }
-
+    

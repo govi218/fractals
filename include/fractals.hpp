edf6f909d67b74ce0547a2a28042092e40fef94a
Pouya Lajevardi <pouya.lajevardi@mail.utoronto.ca>
2021-05-07 14:57:53 -0400

    Major bug fixes
    Loop construct was wrong so that it would only investigate 25% of all the points. Return statement also would create issue so that all investigated points would be included automatically.
    All fixed

--- include/fractals.hpp
+++ include/fractals.hpp
@@ -13,34 +13,41 @@ using FractalSet = std::vector<ComplexDouble>;
 FractalSet m_set;
 
 ComplexDouble mandelbrot_process(ComplexDouble c, int boundary = 2,
-                                 int max_iterations = 32) {
+                                 int max_iterations = 512) {
     using namespace std::complex_literals;
 
     ComplexDouble z(0, 0);
+    ComplexDouble zero_elm(0,0);
     int i = 0;
     while (abs(z) < abs(boundary)) {
         if (i == max_iterations)
-            break;
+            // break;
+            return c;
         z = std::pow(z, 2) + c;
         ++i;
     }
-    return z;
+    return zero_elm;
 }
 
 void mandelbrot(double start_val, double end_val, double step_val) {
     FractalSet temp_set;
+    ComplexDouble m_element;
+    ComplexDouble zero_elm(0,0);
     double step = step_val; // 0.01;
     double start = start_val; //-2.0;
     double end = end_val; //2.0;
     for (double i = start; i < end; i += step) {
-        for (double j = start; j < end; j += step) {
+        for (double j = -2.0; j < 2.0; j += step) {
             ComplexDouble c(i, j);
-            temp_set.push_back(mandelbrot_process(c));
+            m_element = mandelbrot_process(c);
+            if(m_element != zero_elm)
+                temp_set.push_back(m_element);
             }
     }
     // int thread_num = omp_get_thread_num();
     #pragma omp critical
     m_set.insert(m_set.end(), temp_set.begin(), temp_set.end());
+    
     // #pragma omp critical
     // std::cout << "m_set appende by thread " << thread_num <<std::endl;
                 
@@ -50,13 +57,17 @@ void mandelbrot(double start_val, double end_val, double step_val) {
 
 
 FractalSet thread(){
+    ComplexDouble zero_elm(0,0);
     // // Pre loop thread setup
     int num_threads = omp_get_max_threads();
     std::cout << "Number of threads: " << num_threads << std::endl;
     omp_set_num_threads(omp_get_max_threads());
-    // Complexity calc: (4/(8*100)/res)^2  | 100 is the value of break_thread
+    int break_thread = 1000;
+    double res = 0.001;
+    // Complexity calc: (4/res)^2/(break_thread*num_threads)
+    std::cout << "Each break thread investigates " << pow((4/res), 2)/(break_thread*num_threads) << " points." << std::endl;
     // Total steps to take = Max number possible points to evaluate: (4/res)^2
-    int break_thread = 100;
+    std::cout << "Total points to investigate: " << pow(4/res, 2) << std::endl;
     // #pragma omp parallel
     #pragma omp parallel for
     for(int t=0; t<break_thread*num_threads; t++) {
@@ -65,8 +76,10 @@ FractalSet thread(){
         // int thread_num = omp_get_thread_num();
         // #pragma omp critical
         // std::cout << "Thread " << thread_num << " is handling t = " << t << std::endl;
-        mandelbrot(start_here, end_here, 0.0001);
+        mandelbrot(start_here, end_here, res);
     }
+    #pragma omp critical
+    m_set.push_back(zero_elm);
 
     return m_set;
 }
--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -26,7 +26,9 @@ using ComplexDouble = std::complex<double>;
 using FractalSet = std::vector<ComplexDouble>;
 
 int main() {
+    // Setting up compilation time
     auto start = std::chrono::high_resolution_clock::now();
+    // intiate and generate m_set
     auto m_set = fractals::thread();
     std::cout << "m_set size is: " << m_set.size() << std::endl;
     // std::cout << "{ ";
@@ -36,18 +38,18 @@ int main() {
     // std::cout << "}" << std::endl;
 
     // To output the set (vector m_set) into a text file
-
-    // std::ofstream file;
-	// file.open("m_set.txt");
-	// for(int i=0;i<m_set.size();++i){
-	// 	file<<m_set[i]<<" ";
-	// }
-	// file.close();
+    std::ofstream file;
+	file.open("m_set.txt");
+	for(int i=0;i<m_set.size();++i){
+		file<<m_set[i]<<" ";
+	}
+	file.close();
 
     // QCustomPlot custom_plot;
     // custom_plot.addGraph();
     // custom_plot.graph(0)->setPen(QPen(Qt::blue)); // line color blue for
     // first graph custom_plot.graph(0)->setBrush(QBrush(QColor(0, 0, 255,20))); // first graph will be filled with translucent blue
+    // Calculate compile time
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     std::cout << "Runtime is " << duration.count() << " seconds" <<std::endl;
